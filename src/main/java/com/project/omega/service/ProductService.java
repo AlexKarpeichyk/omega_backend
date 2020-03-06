7360fb64e3d5371ba6361cda75e8131d01902b30
Kaylesh <Kaylesh.Patel@live.co.uk>
2020-03-06 15:12:06 +0000

    Added name and price search to product service. Added JPA queries to product repository to reflect new functionality.

--- src/main/java/com/project/omega/service/ProductService.java
+++ src/main/java/com/project/omega/service/ProductService.java
@@ -2,14 +2,19 @@ package com.project.omega.service;
 
 import com.project.omega.bean.Product;
 import com.project.omega.exceptions.NoRecordsFoundException;
+import com.project.omega.exceptions.ProductNotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
 
 boolean createProduct(Product product);
-List<Product> getAllProducts();
-Product getProductById(Long id) throws NoRecordsFoundException;
+List<Product> getAllProducts() throws NoRecordsFoundException;
+List<Product> getProductsAbovePrice(int price) throws NoRecordsFoundException;
+List<Product> getProductsBelowPrice(int price) throws NoRecordsFoundException;
+List<Product> getProductsEqualPrice(int price) throws NoRecordsFoundException;
+Product getProductById(Long id) throws ProductNotFoundException;
+List<Product> getProductsBySearchQuery(String name) throws NoRecordsFoundException;
 boolean updateProduct(Long id, Product newProduct);
 boolean deleteProductById(Long id);
 }
