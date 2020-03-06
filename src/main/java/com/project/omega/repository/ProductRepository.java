b250c8a58d897a25c550369e10bc6602d77dd365
Kaylesh <Kaylesh.Patel@live.co.uk>
2020-03-06 14:06:14 +0000

    Created JPA repository for Product.

---
+++ src/main/java/com/project/omega/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.project.omega.repository;
+
+import com.project.omega.bean.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
